5f31f43f533a82d2f526789bb9e289dc0357546b
Abhishek Gupta <awittlegupta@gmail.com>
2017-07-27 23:35:36 -0700

    exercises

--- postgresql_challenges.sql
+++ postgresql_challenges.sql
@@ -1,6 +1,6 @@
 -- Description:
 --	This is basically a refresher on PostgreSQL and PGAdmin (mostly queries). Queries progressively get more
---  difficult.
+--     difficult.
 
 -- NOTE: All database(s) and table(s) information / schema is stored in the database.tar file.
 
@@ -286,4 +286,197 @@
 			FROM customer
 			WHERE first_name LIKE 'E%' AND address_id < 500
 			ORDER BY customer_ID DESC
+			LIMIT 1; 
+			
+			
+-- 38. [INNER JOIN] Order every customer by first and last name and show how much total money they have spent.
+	
+			SELECT CONCAT(first_name, ' ', last_name) AS "NAME", SUM(amount)
+			FROM customer JOIN payment ON customer.customer_id = payment.customer_id
+			GROUP BY customer.customer_id
+			ORDER BY first_name, last_name ASC;
+			
+			
+-- 39. [INNER JOIN] Print the first and last names of all staff members along with the sum total of all their transactions.
+
+			SELECT staff.staff_id, CONCAT(first_name, ' ', last_name) AS "Name", SUM(amount)
+			FROM staff JOIN payment ON staff.staff_id = payment.staff_id
+			GROUP BY staff.staff_id
+			ORDER BY SUM(amount) DESC;
+			
+			
+-- 40. [INNER JOIN] Figure out how many of each movie is at a store.
+		
+			SELECT store_id, title, COUNT(*) AS "Available"
+			FROM inventory JOIN film ON inventory.film_id = film.film_id 
+			GROUP BY store_id, title 
+			ORDER BY title ASC;
+			
+			
+-- 41. [INNER JOIN] How many of each movie is at store #2?
+
+			SELECT store_id, title, COUNT(*) AS "Available"
+			FROM inventory JOIN film ON inventory.film_id = film.film_id 
+			WHERE store_id = 2
+			GROUP BY store_id, title 
+			ORDER BY title ASC;
+			
+			
+-- 42. [INNER JOIN] List out all films with their corresponding language.
+
+			SELECT title, name
+			FROM film JOIN language ON language.language_id = film.language_id 
+			ORDER BY title ASC;
+			
+			
+-- 43. [LEFT OUTER JOIN] Find every film and its total copies regardless of whether or not it is in inventory.
+
+			SELECT title, COUNT(inventory_id) AS "available"
+			FROM film
+			LEFT JOIN inventory ON film.film_id = inventory.film_id
+			GROUP BY film.film_id
+			ORDER BY "available" ASC;
+			
+			
+-- 44. [LEFT OUTER JOIN] Find every film with a null inventory ID.
+
+			SELECT title, COUNT(inventory_id) AS "available"
+			FROM film
+			LEFT JOIN inventory ON film.film_id = inventory.film_id
+			WHERE inventory_id IS NULL
+			GROUP BY film.film_id
+			ORDER BY title ASC;
+			
+			
+-- 45. Find the total amount by month.
+
+			SELECT EXTRACT(month from payment_date) AS "Month", SUM(amount)
+			FROM payment
+            GROUP BY "Month"
+            ORDER BY "Month" ASC;
+			
+			
+-- 46. Find the films whose rental rate is higher than the average rental rate.
+
+			SELECT title, rental_rate
+			FROM film
+			WHERE rental_rate > (SELECT AVG(rental_rate) FROM film)
+			ORDER BY rental_rate DESC;
+			
+			
+-- 47. Find the title of the films where the return date is between May 29th, 2005 and May 30th, 2005. 
+
+			SELECT film_id, title
+			FROM film 
+			WHERE film.film_id IN 
+
+			(SELECT inventory.film_id 
+			FROM rental 
+			JOIN inventory ON inventory.inventory_id = rental.inventory_id 
+			WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30');
+			
+			
+-- 48. [SELF JOIN] Find all customers whose last name is the same as someone's first_name.
+
+			SELECT CONCAT(t1.first_name, ' ', t1.last_name), CONCAT(t2.first_name, ' ', t2.last_name)
+			FROM customer AS t1, customer AS t2
+			WHERE t1.last_name = t2.first_name;
+			
+			-- OR --
+			
+			SELECT CONCAT(t1.first_name, ' ', t1.last_name), CONCAT(t2.first_name, ' ', t2.last_name)
+			FROM customer AS t1 JOIN customer AS t2 ON t1.last_name = t2.first_name;
+			
+			
+-- 49. Given the following tables: Given an Employee table which has 3 fields - Id (Primary key), Salary and 
+--     Manager Id, where manager id is the id of the employee that manages the current employee, find all 
+--     employees that make more than their manager in terms of salary. 
+
+			SELECT e1.id
+			FROM employee AS e1, employee AS e2
+			WHERE e1.id = e2.manager_id AND e1.salary > e2.salary;
+ 
+ 
+ ---- QUESTIONS 50 to 65 will use a new database called exercises.tar
+ 
+ -- 50. Retrieve everything from the cd.facilities table.
+ 
+			SELECT * 
+			FROM cd.facilities;
+			
+			
+-- 51. Print a list of all of the facilities and their cost to members. Order by greatest cost.
+
+			SELECT name, membercost
+			FROM cd.facilities
+			ORDER BY membercost DESC;
+			
+			
+-- 52. Print a list of facilities that charge a fee to members. Order by highest.
+
+			SELECT name, membercost
+			FROM cd.facilities
+			WHERE membercost > 0
+			ORDER BY membercost DESC;
+			
+			
+-- 53. Print a list of facilities that charge a fee to members. The fee is less than 1/50th of the monthly 
+--     maintenance cost. Print the facid, facility name, member cost, and monthly maintenance of the 
+--     facilities in question.	
+
+			SELECT facid, name, membercost, monthlymaintenance
+			FROM cd.facilities
+			WHERE membercost > 0 AND membercost < (monthlymaintenance/50);
+			
+			
+-- 54. Print the list of all facilities with 'Tennis' in their name.
+
+			SELECT name
+			FROM cd.facilities
+			WHERE name LIKE '%Tennis%';
+			
+			
+--- 55. Print the details of facilities with ID 1 and 5. Try to do it without using the OR operator.
+
+			SELECT *
+			FROM cd.facilities
+			WHERE facid IN (1, 5);
+			
+			
+-- 56. Print the list of members who joined after the start of September 2012. Return 
+--     the memid, surname, firstname, and joindate of the members in question.
+
+			SELECT memid, surname, firstname, joindate
+			FROM cd.members
+			WHERE joindate > '2012-09-01';
+			
+			
+-- 57. Print an ordered list of the first 10 surnames in the members table. The list must not contain duplicates.
+
+			SELECT DISTINCT surname
+			FROM cd.members
+			ORDER BY surname ASC
+			LIMIT 10;
+			
+			
+-- 58. Get the signup date of your last member. 
+
+			SELECT *
+			FROM cd.members
+			ORDER BY joindate DESC
 			LIMIT 1;
+			
+			
+-- 59. Print a count of the number of facilities that have a cost to guests of 10 or more.
+
+			SELECT COUNT(*)
+			FROM cd.facilities
+			WHERE membercost > 10;
+			
+			
+-- 60. Print a list of the total number of slots booked per facility in the month of September 2012. 
+--     Produce an output table consisting of facility id and slots, sorted by the number of slots.
+
+			SELECT name, slots, starttime 
+			FROM cd.facilities AS fac JOIN cd.bookings AS book ON book.facid = fac.facid
+			WHERE starttime BETWEEN '2012-09-01' AND '2012-09-30 23:59:59';
